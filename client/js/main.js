5c66f352a95319907d3eb504f57fcfc03e18f4a9
sharifc2016 <sharifc2016@gmail.com>
2021-10-26 20:17:33 +0600

    open google chrome from browser

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
     var csInterface = new CSInterface();
+    csInterface.openURLInDefaultBrowser("http://www.google.com");
     document.getElementById('btn_test').addEventListener('click', function () {
         csInterface.evalScript('sayHello()');
     });
