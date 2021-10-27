bbc97d5fd5befe671b7bde61b003c5fd1e5a94ca
sharifc2016 <sharifc2016@gmail.com>
2021-10-27 14:44:12 +0600

    data exchange with json module
    JS to JSX and JSX to JS with

--- client/js/main.js
+++ client/js/main.js
@@ -1,15 +1,45 @@
 (function () {
-    'use strict';
     var csInterface = new CSInterface();
-    document.getElementById('btn_test').addEventListener('click', function () {
-        // collect data from jsx
-        csInterface.evalScript('collectData()', function (result) {
-            console.log(result);
+    var includeFile = csInterface.getSystemPath(SystemPath.EXTENSION) +
+    "/host/include/json2.jsx";
+    console.log(includeFile);
+    csInterface.evalScript('evalFile("' + includeFile + '")',
+        function(res) {
+            console.log(res);
+        });
 
-            var scpt = 'showData("' + result + '")';
-            console.log(scpt);
-            // pass data to jsx
-            csInterface.evalScript(scpt);
+    // Set the TextArea with a placeholder Object
+    var obj = {
+        str: "Snarky Puppy",
+        num: 42
+    }
+    var sampleObj = JSON.stringify(obj);
+    $("#textarea1").val(sampleObj);
+
+    // JS to JSX button handler
+    $("#sendButton").on('click', function() {
+    // Ugly workaround to grab the textarea code and build an Object out of it.
+    var obj = eval("(" + $("#textarea1").val() + ")");
+    // Use JSON.stringify to massage the object before sending it down to the JSX
+    csInterface.evalScript('JSXParseObj(' + JSON.stringify(obj) + ')',
+        function(res) {
+            console.log(res);
         });
     });
+
+
+    // JSX to JS button handler
+    $("#getButton").on('click', function() {
+    // Tell the JSX to send up to the JS an object
+    csInterface.evalScript('sendObjectToJS()', function(res) {
+        var stringToShow = "In order to use the following object," +
+        " you need to JSON.parse() it!\n\n" + res;
+        $("#textarea2").val(stringToShow);
+        console.log(res);
+    });
+    });
+
+
+
+
 }());
--- host/index.jsx
+++ host/index.jsx
@@ -1,7 +1,25 @@
-function collectData() {
-    return 'data from jsx';
+function evalFile(path) {
+    try {
+        $.evalFile(path);
+    } catch (e) {
+        alert("Exception:" + e);
+    }
+}
+// Parse the Object that comes from JS
+function JSXParseObj(o) {
+    var str = "Object received!\n";
+    for (prop in o) {
+        str += prop + " [" + typeof o[prop] + "]: " + o[prop] + ".\n";
+    }
+    alert(str);
 }
 
-function showData(data) {
-    alert(data);
+// Create an object and return the
+// JSON stringified version of it
+function sendObjectToJS () {
+    var obj = {
+        str: "Snarky Puppy",
+        num: 42
+    }
+    return JSON.stringify(obj);
 }
